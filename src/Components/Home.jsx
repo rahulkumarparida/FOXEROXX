044119cc43f205a307cf3ae7869db5c0b4dc003e
rahulkumarparida <rahulkumarparida52@gmail.com>
2025-05-01 11:46:57 +0530

    multiple inputs to be taken  done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -28,10 +28,10 @@ export default function(){
     return <div className="flex items-center h-[95vh] border-1 flex-col ">
                  <h1 className="pt-4 text-3xl font-[sans]">HomePage</h1>   
         <div className="HomeConatiner py-10 flex  flex-col  items-center">
-            <div className="CustomGroupQ shadow-2xl h-20 w-220 border-1 flex items-center justify-between px-10 rounded-md">
+            {/* <div className="CustomGroupQ shadow-2xl h-20 w-220 border-1 flex items-center justify-between px-10 rounded-md">
                     <p className=" text-xl">Make your custom Group for xeroxxing!!!</p>
-                    <button className="createBTN border-1 shadow-md p-1 rounded-sm ">CREATE</button>
-            </div>
+                    <button className="createBTN border-1 shadow-md p-1 rounded-sm " onClick={()=>{click ? setClick(false) : setClick(true)}}>CREATE</button>
+            </div> */}
 
             {click ?  <MemberEntryFrom />   :  <MemberCard Card={Card} />}
         </div>
--- src/Components/Ui/MemberEntryForm.jsx
+++ src/Components/Ui/MemberEntryForm.jsx
@@ -1,16 +1,82 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function MemberEntryFrom() {
-    let [groupData, setGroupData] = useState()
-    return <div className="FormCont  border-1 rounded-sm  h-110 w-300 mt-10 flex pt-4 px-8">
-        <form >
-        <input className="w-100" type="text" placeholder="Enter the Group Name" />
-        <div className="pt-4">
-        <input className="" type="text" placeholder="Group Leader Name" />
-        <input className="" type="text" placeholder="Total Members " />
-        <button className="border-1 shadow-md mx-2 p-[3px] px-3 rounded-sm">ADD</button>
-        </div>
+    // let [groups, setAnotherGroup] = useState([]); 
+    let [done ,setDone] = useState(false)
+  let [group, setGroup] = useState({
+    GroupName: "",
+    LeaderName: "",
+    TotalMembers: 0,
+    Data: undefined,
+  });
+  let arr = Array(group.TotalMembers).fill({})
+  let [member , setMember ] = useState({Name :"" , Amount : 0})
+  function handleClick(e) {
+    console.log(group);
+    console.log(`arr : ${ arr }`);
+    
+   
+        // setGroup({...group , Data : [...data  , member]})
 
-        </form>
+        // setMember({Name :"" , Amount : 0 }) 
+        
+    
+   setDone(true)
+     
+    e.preventDefault();
+  }
+
+ 
+  return (
+    <div className="FormCont  border-1 rounded-sm  mt-10 flex p-4 px-8">
+      <form>
+        <input
+          className="w-100"
+          type="text"
+          placeholder="Enter the Group Name"
+          onChange={(e) => {
+            setGroup({ ...group, GroupName: e.target.value });
+          }}
+        />
+        <div className="pt-4">
+          <input
+            className=""
+            type="text"
+            placeholder="Group Leader Name"
+            onChange={(e) => {
+              setGroup({ ...group, LeaderName: e.target.value });
+            }}
+          />
+          <input
+            className=""
+            type="number"
+            placeholder="Total Members "
+            onChange={(e) => {
+              setGroup({ ...group, TotalMembers: Number(e.target.value) });
+            }}
+          />
+          <button
+            className="border-1 shadow-md mx-2 p-[3px] px-3 rounded-sm"
+            onClick={(e) => {
+              handleClick(e);
+            }}
+          >
+            ADD
+          </button>
+        </div>
+        <div className="teaMinputs ">
+            {
+             done ?  arr.map((ele ,idx)=>{
+                return <div className="border-1 ">
+                       <input type="text" placeholder="Name" onChange={()=>{setMember({...member , Name : e.target.value})}}  />
+                       <input type="text" placeholder="Amount" onChange={()=>{setMember({...member , Amount : e.target.value})}} /> 
+                </div>
+              }):""
+
+                
+            }
+        </div>
+      </form>
     </div>
+  );
 }
